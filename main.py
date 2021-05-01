c0da6bf895760c12435d870f70f5826fc9fbcd40
Cyan_Heron <templar.of.silence@gmail.com>
2021-05-01 13:53:28 +0200

    train/val/test set splitting image + label extraction

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import tensorflow as tf
+import os
+import argparse
+import numpy as np
+import cv2
 
 
-# Press the green button in the gutter to run the script.
+def files_to_numpy(x, dirs):
+    res = [[cv2.imread(os.path.join(d, i + t)) for i in x] for d, t in dirs]
+    return res
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--dataset', default='../dataset')
+    parser.add_argument('--labels', default='../labels')
+    parser.add_argument('--val_split', default=0.15)
+    parser.add_argument('--test_split', default=0.15)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    args = parser.parse_args()
+
+    np.random.seed(42)
+
+    _folder_dataset_names  = set([os.path.splitext(f)[0] for f in os.listdir(args.dataset) if f.endswith(('.jpg', '.png'))])
+    _folder_dataset_labels = set([os.path.splitext(f)[0] for f in os.listdir(args.labels ) if f.endswith(('.jpg', '.png'))])
+
+    _main_dataset_names = np.array(list(_folder_dataset_labels))
+    _extra_dataset_names = np.array(list(_folder_dataset_names - _folder_dataset_labels))
+
+    np.random.shuffle(_main_dataset_names)
+    _main_length = len(_main_dataset_names)
+
+    _test_length = int(args.test_split * _main_length)
+    _val_length = int(args.val_split * _main_length)
+    _train_length = _main_length - _test_length - _val_length
+
+    _train_split_names = _main_dataset_names[:_train_length]
+    _eval_split_names = _main_dataset_names[_train_length:_train_length+_val_length]
+    _test_split_names = _main_dataset_names[_train_length+_val_length:]
+
+    x_train, y_train = files_to_numpy(
+        _train_split_names,
+        [(args.dataset, '.jpg'), (args.labels, '.png')]
+    )
+
+    h=0
+
